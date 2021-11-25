bf107ae8bdd0a5e9827e258e38f598cc5ea79727
Misterloose <95049543+Misterloose@users.noreply.github.com>
2021-11-25 20:12:37 +0100

    Add files via upload

---
+++ Foe.java
@@ -0,0 +1,13 @@
+package runner;
+
+import javafx.geometry.Rectangle2D;
+
+public class Foe extends staticThing{
+
+    public Foe(double x, double y,double size_window_x, double size_window_y ,String FileName) {
+        super(x,y,FileName);
+//        super(x, y, 0, size_window_x,size_window_y, 0, 0, "still", FileName);
+//        this.getImage().setViewport(new Rectangle2D(x,y,size_window_x,size_window_y));
+//
+    }
+}
